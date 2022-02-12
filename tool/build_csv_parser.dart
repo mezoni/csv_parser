2f74585be7c70a23bd5376cb544c99505b700043
mezoni <andrew.mezoni@gmail.com>
2022-02-12 16:08:37 +0500

    0.1.0

--- tool/build_csv_parser.dart
+++ tool/build_csv_parser.dart
@@ -23,6 +23,13 @@ const __header = r'''
 
 import 'package:source_span/source_span.dart';
 
+/// Parses the CSV data and returns the result as a `List<List<String>>`.
+/// - Will not parse numbers
+/// - The character `,` is used as a field separator
+/// - Line endings are `\n` or `\r\n`
+/// - The start and end of strings is the character `"`
+/// - Escaping a character `"` in a string is parsed via sequence `""`
+/// - Exception `FormatException` will be thrown if parsing fails
 List<List<String>> parse(String source) {
     final state = StringState(source);
   final result = _parse(state);
